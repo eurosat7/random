c638a2e13abb6d095ba3e8ba68b92ae6ac92716c
eurosat7 <eurosat7@yahoo.de>
2024-01-07 14:25:29 +0100

    generate() is almost unbiased now. Added generateBiased() if you want each set of chars used evenly.

--- src/Generator.php
+++ src/Generator.php
@@ -25,6 +25,8 @@ class Generator
     }
 
     /**
+     * Tries to get one of each set then the rest is unbiased
+     *
      * @param array<int, array<int, string>> $sets
      *
      * @throws RandomException
@@ -35,18 +37,45 @@ class Generator
     {
         $result = [];
         $count = 0;
+        foreach ($sets as $set) {
+            $count++;
+            $result[] = Arrays::pickOne($set);
+        }
+        $superset = array_merge(...$sets);
+        while ($count < $length) {
+            $count++;
+            $result[] = Arrays::pickOne($superset);
+        }
+        if ($shuffle) {
+            $result = Shuffle::shuffle($result);
+        }
+        $result = array_slice($result, 0, $length);
+        return \implode('', $result);
+    }
+
+    /**
+     * Tries to use each set evenly, the length of each set does not matter
+     *
+     * @param array<int, array<int, string>> $sets
+     *
+     * @throws RandomException
+     *
+     * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
+     */
+    public static function generateBiased(array $sets, int $length, bool $shuffle = true): string
+    {
+        $result = [];
+        $count = 0;
         while ($count < $length) {
             $count++;
             foreach ($sets as $set) {
                 $result[] = Arrays::pickOne($set);
-                // we do not remove used elements!
             }
         }
         if ($shuffle) {
             $result = Shuffle::shuffle($result);
         }
         $result = array_slice($result, 0, $length);
-        // no condition in the loop is faster
         return \implode('', $result);
     }
 
@@ -55,7 +84,7 @@ class Generator
      */
     public static function password(int $length = 16): string
     {
-        return self::generate(
+        return self::generateBiased(
             sets: [
                 Charsets::numeric(),
                 Charsets::alphanumeric(),
