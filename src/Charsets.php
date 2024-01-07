d3f416f4d2ec628d56dee2d66d0de52427d1f8cb
eurosat7 <eurosat7@yahoo.de>
2024-01-07 14:21:59 +0100

    added hexadecimal charset

--- src/Charsets.php
+++ src/Charsets.php
@@ -12,6 +12,18 @@ class Charsets
     /**
      * @return array<int, string>
      */
+    public static function hexadecimal(): array
+    {
+        return // range('0', 'f');
+            [
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f'
+            ];
+    }
+
+    /**
+     * @return array<int, string>
+     */
     public static function numeric(): array
     {
         return // range('0', '9');
